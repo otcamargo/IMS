9ae821c4d217e1140450cbe204d2766c82341384
Otavio Camargo <otavioacamargo@outlook.com>
2020-10-25 02:19:09 -0300

    Add cadvisor configs

--- docker-setup.ps1
+++ docker-setup.ps1
@@ -1,14 +1,16 @@
 Clear-Host
 
-Write-Host "[1] start prometheus and grafana"
+Write-Host "[1] start prometheus, grafana and cadvisor"
 
 $Input = Read-Host 
 
 if ($input -eq 1) {
   Write-Host "----------------- Starting prometheus -----------------"
-  docker run --network="data-gather-service_default" -d --name prometheus -p 9090:9090 -v D:\Otavi\Workspace\repos-that-need-fixing\prometheus\IMS\prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus --config.file=/etc/prometheus/prometheus.yml
+  docker run --network="location-service_default" -d --name prometheus -p 9090:9090 -v D:\Otavi\Workspace\repos-that-need-fixing\prometheus\IMS\prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus --config.file=/etc/prometheus/prometheus.yml
   Write-Host "----------------- Starting grafana -----------------"
-  docker run --network="data-gather-service_default" -d --name grafana -p 3000:3000 grafana/grafana
+  docker run --network="location-service_default" -d --name grafana -p 3000:3000 grafana/grafana
+  Write-Host "----------------- Starting cadvisor -----------------"
+  docker run --network="location-service_default" --volume /:/rootfs:ro --volume /var/run:/var/run:rw --volume /sys:/sys:ro --volume /var/lib/docker/:/var/lib/docker:ro -d --name cadvisor -p 8080:8080 google/cadvisor
   docker container ls
 }
 else {
