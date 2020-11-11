72e4608dd4a6ff1da7d5532a587df67c69d11f33
Otavio Camargo <otavioacamargo@outlook.com>
2020-11-11 00:47:15 -0300

    test 2

--- setup.prometheus_2.sh
+++ setup.prometheus_2.sh
@@ -1,5 +1,32 @@
 sudo tee /etc/systemd/system/prometheus.service<<EOF
 
+
+[Unit]
+Description=Prometheus
+Documentation=https://prometheus.io/docs/introduction/overview/
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User=prometheus
+Group=prometheus
+ExecReload=/bin/kill -HUP $MAINPID
+ExecStart=/usr/local/bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/var/lib/prometheus \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=0.0.0.0:9090 \
+  --web.external-url=
+
+SyslogIdentifier=prometheus
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 for i in rules rules.d files_sd; do sudo chown -R prometheus:prometheus /etc/prometheus/${i}; done
 for i in rules rules.d files_sd; do sudo chmod -R 775 /etc/prometheus/${i}; done
-sudo chown -R prometheus:prometheus /var/lib/prometheus/
+sudo chown -R prometheus:prometheus /var/lib/prometheus/ls
