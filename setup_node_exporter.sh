8c14572092422af9af1b08b57f1863bf91f973cc
Otavio Camargo <otavioacamargo@outlook.com>
2020-11-11 01:43:00 -0300

    tests 3

---
+++ setup_node_exporter.sh
@@ -0,0 +1,40 @@
+cd ~/Downloads
+
+curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest \
+| grep browser_download_url \
+| grep linux-amd64 \
+| cut -d '"' -f 4 \
+| wget -qi -
+
+tar -xvf node_exporter*.tar.gz
+cd  node_exporter*/
+sudo cp node_exporter /usr/local/bin
+
+node_exporter --version
+
+sudo tee /etc/systemd/system/node_exporter.service <<EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=default.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
+sudo systemctl status node_exporter
+
+sudo tee /etc/prometheus.yml <<EOF
+  - job_name: 'node_exporter'
+      static_configs:
+        - targets: ['localhost:9100']
+EOF
+
+sudo systemctl restart prometheus
